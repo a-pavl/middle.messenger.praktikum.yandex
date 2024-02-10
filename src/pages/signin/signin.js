180b32467adb7fce86362c4db7ea68bf3f71f282
Alexey Pavlovskiy <a_pavlovskiy@icloud.com>
2024-02-10 14:06:36 +0100

    Добавил заглушки на сабмит форм

--- src/pages/signin/signin.js
+++ src/pages/signin/signin.js
@@ -1,8 +1,14 @@
 import './signin.scss';
 
+// temporarily mock form submittion
+function submitForm(event) {
+    event.preventDefault();
+}
+
 function redirectToLoginPage(event) {
     event.preventDefault();
     location.href = '../login/login.html';
 }
 
+document.getElementById('confirmButton').addEventListener('click', submitForm);
 document.getElementById('redirectButton').addEventListener('click', redirectToLoginPage);
